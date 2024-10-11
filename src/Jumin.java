b6bdf596d5195eb2445e3e499b9e2d14343183b4
jwb6739 <jwb6739@gmail.com>
2024-10-11 11:17:35 +0900

    과제 4. 주민등록번호 생성 프로그램(예외처리 X)

---
+++ src/Jumin.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Jumin {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println("[주민등록번호 계산]");
+        System.out.print("출생년도를 입력해 주세요.(yyyy):");
+        String TempYear = sc.next();
+        String year = TempYear.substring(2, 4);
+
+        System.out.print("출생월을 입력해 주세요.(mm):");
+        int TempMonth = sc.nextInt();
+        String month = null;
+        if (TempMonth < 10) {
+            month = String.format("%02d", TempMonth);
+        }
+
+        System.out.print("출생일을 입력해 주세요.(dd):");
+        String day = sc.next();
+
+        System.out.print("성별을 입력해 주세요.(m/f):");
+        String gender = sc.next();
+        if("m".equals(gender)){
+            gender = "3";
+        } else if ("f".equals(gender)) gender = "4";
+
+        Random rd = new Random();
+        int random = rd.nextInt(999999) + 1; // 1부터 999998 사이의 숫자 생성
+        String TempNum = String.format("%06d", random); // 숫자를 6자리로 변환, 빈 자리는 0으로 채움
+
+        String JuminNum = (sb.append(year).append(month).append(day).append("-").append(gender).append(TempNum)).toString();
+
+        System.out.println(JuminNum);
+    }
+}

27c57565615b33cac85cda0df9988ca123a0f624
WonBin JEON <jwb6739@gmail.com>
2024-10-14 00:43:25 +0900

    과제 8

---
+++ src/CalculateTax.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CalculateTax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("[과세금액 계산 프로그램]");
+        System.out.print("연소득을 입력해 주세요.:");
+
+    }
+}

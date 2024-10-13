a245e23b186bf72cdafa79da8b584d630e65afc8
WonBin JEON <jwb6739@gmail.com>
2024-10-14 00:16:28 +0900

    FEAT : 주석 수정

--- src/CashBack.java
+++ src/CashBack.java
@@ -5,9 +5,11 @@ public class CashBack {
 
         System.out.println("[캐시백 계산]");
         System.out.print("결제 금액을 입력해 주세요.(금액):");
+
         Scanner sc = new Scanner(System.in);
         int cash = sc.nextInt();
 
+        // 캐시백 포인트가 최대 300, 그 이하는 백원 단위
         int cashBack = (int)(cash * 0.1) > 300 ? 300 : (((int)(cash * 0.1) / 100) * 100);
 
         System.out.println(String.format("결제 금액은 %d원이고, 캐시백은 %d원 입니다.", cash, cashBack));
